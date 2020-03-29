70193f103cbc650a0d955da63db9ada3c48f1f5d
Krutika Ankit <ankitparmar14190@gmail.com>
2020-03-29 17:19:15 +0530

    First Source

---
+++ app.js
@@ -0,0 +1,8 @@
+import myApiKey, { apiToken, host as local, greet, address, london } from './src/config';
+
+console.log(myApiKey);
+console.log(apiToken);
+console.log(local)
+greet('Ankit')
+console.log(address);
+console.log(london);
---
+++ src/config.js
@@ -0,0 +1,17 @@
+const apiKey = 'hsdsd33klsd0239d';
+
+export const apiToken = 'sdfk3302cs5/sld3';
+export const host = 'localhost';
+
+export default apiKey;
+
+export function greet(name) {
+     console.log(`Hello ${name}`);
+}
+
+const address = 'afksdkl sfksdjf';
+const city = 'sdkjsdsff';
+export {
+     address,
+     city as london
+}
